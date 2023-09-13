458b9f5ee91c5a30fa170f9d49ce6fd81b6fa6d1
Logan Saso <logansaso@gmail.com>
2023-09-12 17:02:04 -0700

    WIP

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
-use boxcars::{ParseError, Replay};
+use std::collections::HashMap;
 use std::error;
 use std::fs;
-use std::io::{BufReader, Read, Write};
+use std::io::{BufReader, Read};
 use std::path::PathBuf;
+use boxcars::Attribute;
+use boxcars::NewActor;
+
 use clap::Parser;
-use serde_json;
 
 #[derive(Parser, Debug)]
 #[command(author, version, about)]
@@ -19,7 +21,7 @@ fn main() -> Result<(), Box<dyn error::Error>> {
     let mut f = BufReader::new(fs::File::open(&args.replay)?);
 
     let mut replay_data = vec![];
-    let read_bytes = f.read_to_end(&mut replay_data)?;
+    let _read_bytes = f.read_to_end(&mut replay_data)?;
     let replay = boxcars::ParserBuilder::new(&replay_data)
         .must_parse_network_data()
         .parse()?;
@@ -28,9 +30,65 @@ fn main() -> Result<(), Box<dyn error::Error>> {
     // let mut outf = fs::File::create("output.json")?;
     // outf.write_all(json_data.as_bytes())?;
 
-    for frame in replay.network_frames?.frames {
-        println!("Frame: {:?}", frame);
-        break
+    println!("Entities: {:?}", replay.class_indices);
+    let objects_by_class_index = replay.class_indices.iter().fold(HashMap::new(), |mut map, index| {
+        map.insert(index.index, index.class.as_str());
+        map
+    });
+
+    println!("Player: {:?}", replay.objects[147]);
+    let mut player_actors: HashMap<i32, String> = HashMap::new();
+    let mut all_actors: HashMap<i32, boxcars::UpdatedAttribute> = HashMap::new();
+
+    // Unwrap is safe because of parser's `.must_parse_network_data`
+    let mut i = 0;
+    for frame in replay.network_frames.unwrap().frames {
+        // for actor in frame.new_actors {
+        //     println!("Actor: {:?}", actor);
+        //     println!("Object: {:?}", replay.objects[actor.object_id.0 as usize]);
+        //     if let Some(name_id) = actor.name_id {
+        //         println!("Name: {:?}", replay.names[name_id as usize]);
+        //     }
+        // }
+        for actor in frame.updated_actors {
+            if actor.object_id.0 == 168 {
+                if let Attribute::String(player_name) = &actor.attribute {
+                    println!("Adding Actor to Map: {:?}", actor);
+                    player_actors.insert(actor.actor_id.0,player_name.clone());
+                }
+            }
+
+            if let Attribute::ActiveActor(active) = &actor.attribute {
+                if !all_actors.contains_key(&actor.actor_id.0) {
+                    all_actors.insert(actor.actor_id.0, actor.clone());
+                }
+            }
+
+
+            // if player_actors.get(&actor.actor_id.0).is_some() {
+            //     println!("Actor: {:?}", actor);
+            // }
+
+            if let Attribute::RigidBody(body) = &actor.attribute {
+                // match player_actors.get(&actor.actor_id.0) {
+                //     Some(name) => println!("Player Moved: {:?}", body),
+                //     None => println!("Unknown Actor Moved: {:?}", actor),
+                // }
+                match all_actors.get(&actor.actor_id.0) {
+                    Some(&boxcars::UpdatedAttribute { attribute: Attribute::ActiveActor(actor_id), .. }) => {
+                        if let Some(player) = player_actors.get(&actor_id.actor.0) {
+                            println!("{} Moved: {:?}", player, actor.attribute)
+                        }
+                    },
+                    _ => {},
+                    // _ => println!("Unknown Actor Moved: {:?}", actor),
+                }
+
+
+            }
+            // println!("Actor: {:?}", actor);
+            // println!("Object: {:?}", replay.objects[actor.object_id.0 as usize]);
+        }
     }
 
     Ok(())
